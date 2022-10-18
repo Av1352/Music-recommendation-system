93c503fd90f54cb223c1782218d4f56477c38dee
Av1352 <66872073+Av1352@users.noreply.github.com>
2022-10-18 23:35:42 +0530

    emotion recognised

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import tkinter
+import cv2
 from tkinter import *
 from PIL import Image, ImageTk
-import face_capture
-import emotion_recognition
+
+from deepface import DeepFace
+from fer import FER
+import face_capture as face
 
 root = Tk()
 
@@ -11,21 +13,15 @@ root.title("Welcome to Music Recommendation")
 root.minsize(width=400, height=400)
 root.geometry("600x500")
 
+def clicked():
+    face.capture()    
+    img = cv2.imread("user.png")
+    predictions = DeepFace.analyze(img)
+    print(predictions['dominant_emotion'])
+    lbl.configure(text=predictions['dominant_emotion'])
+
 lbl = Label(root, text="What is your mood")
 lbl.grid()
-
-
-def clicked():
-    face_capture.capture()
-    emotion = emotion_recognition.predictions
-    lbl.configure(text=emotion['dominant_emotion'])
-    # image1 = Image.open("user.png")
-    # test = ImageTk.PhotoImage(image1)
-    # label1 = tkinter.Label(image=test)
-    # label1.image = test
-    # label1.place(x = 0 , y = 0)
-
-
 btn = Button(root, text="Click me",
              fg="red", command=clicked)
 btn.grid(column=1, row=0)
--- emotion_recognition.py
+++ emotion_recognition.py
@@ -1,8 +1,2 @@
-import cv2
-from deepface import DeepFace
-import matplotlib.pyplot as plt
-from fer import FER
 
-img = cv2.imread("user.png")
-predictions = DeepFace.analyze(img)
-print(predictions['dominant_emotion'])
+
