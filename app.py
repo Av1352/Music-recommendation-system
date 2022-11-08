3cd633d8694d1e1ef64ec8a470a70eff30e7b047
Av1352 <66872073+Av1352@users.noreply.github.com>
2022-11-08 23:47:55 +0530

    hello

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import cv2
 # from deepface import DeepFace
 from fer import FER
 import face_capture as face
+import camera as camera
 
 app = Flask(__name__)
 app_root = os.path.abspath(os.path.dirname(__file__))
@@ -21,7 +22,7 @@ def index():
 def click():
 	face.capture()
 	img = cv2.imread("user.png")
-	
+
 	predictions = DeepFace.analyze(img)
 	print(predictions)
 	
--- camera.py
+++ camera.py
@@ -11,52 +11,54 @@ from pandastable import Table, TableModel
 from tensorflow.keras.preprocessing import image
 import datetime
 from threading import Thread
-# from Spotipy import *  
+
 import time
 import pandas as pd
-face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-ds_factor=0.6
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+ds_factor = 0.6
 
-emotion_model = Sequential()
-emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
-emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
-emotion_model.add(Dropout(0.25))
-emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
-emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
-emotion_model.add(Dropout(0.25))
-emotion_model.add(Flatten())
-emotion_model.add(Dense(1024, activation='relu'))
-emotion_model.add(Dropout(0.5))
-emotion_model.add(Dense(7, activation='softmax'))
-emotion_model.load_weights('model.h5')
 
-cv2.ocl.setUseOpenCL(False)
+def camera():
+    emotion_model = Sequential()
+    emotion_model.add(Conv2D(32, kernel_size=(
+        3, 3), activation='relu', input_shape=(48, 48, 1)))
+    emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+    emotion_model.add(Dropout(0.25))
+    emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+    emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+    emotion_model.add(Dropout(0.25))
+    emotion_model.add(Flatten())
+    emotion_model.add(Dense(1024, activation='relu'))
+    emotion_model.add(Dropout(0.5))
+    emotion_model.add(Dense(7, activation='softmax'))
+    emotion_model.load_weights('model.h5')
 
-emotion_dict = {0:"Angry",1:"Disgusted",2:"Fearful",3:"Happy",4:"Neutral",5:"Sad",6:"Surprised"}
-music_dist={0:"songs/angry.csv",1:"songs/disgusted.csv ",2:"songs/fearful.csv",3:"songs/happy.csv",4:"songs/neutral.csv",5:"songs/sad.csv",6:"songs/surprised.csv"}
-                                  
-show_text=[0]
+    cv2.ocl.setUseOpenCL(False)
 
-image = cv2.imread("user.png")
-image=cv2.resize(image,(600,500))
-gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-face_rects=face_cascade.detectMultiScale(gray,1.3,5)
-df1 = pd.read_csv(music_dist[show_text[0]])
-df1 = df1[['Name','Album','Artist']]
-df1 = df1.head(15)
-for (x,y,w,h) in face_rects:
-	cv2.rectangle(image,(x,y-50),(x+w,y+h+10),(0,255,0),2)
-	roi_gray_frame = gray[y:y + h, x:x + w]
-	cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
-	prediction = emotion_model.predict(cropped_img)
-	print(prediction)
+    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful",
+                    3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+    music_dist = {0: "songs/angry.csv", 1: "songs/disgusted.csv ", 2: "songs/fearful.csv",
+                  3: "songs/happy.csv", 4: "songs/neutral.csv", 5: "songs/sad.csv", 6: "songs/surprised.csv"}
 
-# def music_rec():
-# 	# print('---------------- Value ------------', music_dist[show_text[0]])
-# 	df = pd.read_csv(music_dist[show_text[0]])
-# 	df = df[['Name','Album','Artist']]
-# 	df = df.head(15)
-# 	return df
+    show_text = [0]
+    image = cv2.imread("user.png")
+    image = cv2.resize(image, (600, 500))
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face_rects = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in face_rects:
+        cv2.rectangle(image, (x, y-50), (x+w, y+h+10), (0, 255, 0), 2)
+        roi_gray_frame = gray[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(
+            cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+        prediction = emotion_model.predict(cropped_img)
+
+    maxindex = int(np.argmax(prediction))
+	return emotion_dict[maxindex]
+
+if __name__ == '__main__':
+    camera()
