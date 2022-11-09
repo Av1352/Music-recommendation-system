5b75070f87c5869214472da5295d4d4ae24e9e80
A Yegu Sanjana <76221871+yegusanjana@users.noreply.github.com>
2022-11-09 20:35:20 +0530

    theteh

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import render_template, request, url_for, redirect, session, make_res
 import os
 import cv2
 import pandas as pd
-from pandas import DataFrame, read_csv
+from pandas import DataFrame , read_csv
 from fer import FER
 import face_capture as face
 import camera as camera
@@ -14,52 +14,35 @@ app_root = os.path.abspath(os.path.dirname(__file__))
 app.secret_key = os.urandom(10)
 
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
     return render_template('index.html')
 
-
 @app.route('/click')
 def click():
-    face.capture()
-    img = cv2.imread("user.png")
-
-    # predictions = DeepFace.analyze(img)
-    # print(predictions)
-    prediction = camera.camera()
-    print(prediction)
-
-    global emotion
-    emotion = prediction
-
-    return render_template('index.html', item=prediction)
-
+	face.capture()
+	img = cv2.imread("user.png")
+	
+	prediction = camera.camera()
+	print(prediction)
+	
+	global emotion
+	emotion = prediction
+	
+	return render_template('index.html', item=prediction)
 
 @app.route('/generate')
 def gen_table():
-    #headings = ("Name","Album","Artist")
+   
     df1 = camera.music_rec()
     df1 = df1.head(15)
     print(df1)
     name = df1["Name"].to_numpy()
     album = df1["Album"].to_numpy()
     artist = df1["Artist"].to_numpy()
-    print(name1)
-    print(album)
-    print(artist)
-    # items = []
-    # for i in range(1, 11):
-    # 	i = df1(i)
-    # 	an_item = dict(df1.name, df1.artist ,df1.album)
-    # 	items.append(an_item)
-    # titles = df1.columns.values
-    # print(titles) 
-	
     return render_template('generate.html', name=name, album=album, artist=artist)
 
-
-# tables=[df1.to_html(classes='data')]
-# df1.to_html(header="true", table_id="table")
 if __name__ == '__main__':
     app.run(debug=True)
