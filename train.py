2094233be6c14101af49fd7025727f20ec6873b0
Av1352 <66872073+Av1352@users.noreply.github.com>
2022-11-12 01:47:18 +0530

    fin

--- train.py
+++ train.py
@@ -13,33 +13,34 @@ val_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
     train_dir,
-    target_size = (48,48),
-    batch_size = 64,
-    color_mode = "grayscale",
-    class_mode = 'categorical'
+    target_size=(48, 48),
+    batch_size=64,
+    color_mode="grayscale",
+    class_mode='categorical'
 )
 
 val_generator = val_datagen.flow_from_directory(
     val_dir,
-    target_size = (48,48),
-    batch_size = 64,
-    color_mode = "grayscale",
-    class_mode = 'categorical'
+    target_size=(48, 48),
+    batch_size=64,
+    color_mode="grayscale",
+    class_mode='categorical'
 )
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 emotion_model = Sequential()
 
-emotion_model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape = (48,48,1)))
-emotion_model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
-emotion_model.add(MaxPooling2D(pool_size=(2,2)))
+emotion_model.add(Conv2D(32, kernel_size=(
+    3, 3), activation='relu', input_shape=(48, 48, 1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
 emotion_model.add(Dropout(0.25))
 
-emotion_model.add(Conv2D(128, kernel_size=(3,3), activation='relu'))
-emotion_model.add(MaxPooling2D(pool_size=(2,2)))
-emotion_model.add(Conv2D(128, kernel_size=(3,3), activation='relu'))
-emotion_model.add(MaxPooling2D(pool_size=(2,2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
 emotion_model.add(Dropout(0.25))
 
 emotion_model.add(Flatten())
@@ -47,14 +48,15 @@ emotion_model.add(Dense(1024, activation='relu'))
 emotion_model.add(Dropout(0.5))
 emotion_model.add(Dense(7, activation='softmax'))
 
-emotion_model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
+emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(
+    lr=0.0001, decay=1e-6), metrics=['accuracy'])
 
 emotion_model_info = emotion_model.fit_generator(
     train_generator,
-    steps_per_epoch = 28709 // 64,
+    steps_per_epoch=28709 // 64,
     epochs=75,
-    validation_data = val_generator,
-    validation_steps = 7178 // 64
+    validation_data=val_generator,
+    validation_steps=7178 // 64
 )
 
 emotion_model.save_weights('model.h5')
